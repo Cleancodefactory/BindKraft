3dde1654cc033f4f61684bc86ff68e77d4b88dd3
Michael Palazov <mpalazov@cleancodefactory.de>
2019-04-23 19:20:10 +0300

    Extended the syntax accepted by TemplateConnector to support list of template specifications for chained fall-back. E.g new TemplateConnector("module1/tml1,module2/tmlX");

--- Scripts/core/connectors/TemplateConnector.js
+++ Scripts/core/connectors/TemplateConnector.js
@@ -3,7 +3,13 @@ function TemplateConnector(addr, host, options) {
 }
 TemplateConnector.Inherit(Connector, "TemplateConnector");
 TemplateConnector.prototype.resolve = function(callback) {
-	var tml = ITemplateSourceImpl.GetGlobalTemplate(ITemplateSourceImpl.ParseTemplateName(this.$data));
+	var addr = this.$data + "";
+	var arr = addr.split(",");
+	var tml = null;
+	for (var i = 0; i < arr.length;i++) {
+		tml = ITemplateSourceImpl.GetGlobalTemplate(ITemplateSourceImpl.ParseTemplateName(arr[i].trim()));
+		if (tml != null) break;
+	}
 	if (tml != null) {
 		this.$resource = tml;
 		return true;
