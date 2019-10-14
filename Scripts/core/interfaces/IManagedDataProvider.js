a2b4ea6e85601950875d6a060d46977c8c09b0aa
Michael Palazov <mpalazov@cleancodefactory.de>
2019-10-14 13:26:56 +0300

    ...

--- Scripts/core/interfaces/IManagedDataProvider.js
+++ Scripts/core/interfaces/IManagedDataProvider.js
@@ -5,4 +5,4 @@ IManagedDataProvider.Interface("IManagedDataProvider","IManagedInterface");
 /**
 	See comments in the  IManagedDataSupplier
 */
-IManagedDataProvider.prototype.GetSupplier = function(parameters) { throw "not impl";}.ReturnType(IManagedDataSupplier);
+IManagedDataProvider.prototype.GetSupplier = function(parameters, orders) { throw "not impl";}.ReturnType(IManagedDataSupplier);
--- Scripts/core/interfaces/IManagedDataSupplier.js
+++ Scripts/core/interfaces/IManagedDataSupplier.js
@@ -4,6 +4,7 @@ IManagedDataSupplier.Interface("IManagedDataSupplier",IManagedInterface);
 	@offset
 	@limit
 	@parameters {object}	Optional. If not supported exception must be thrown (see the remarks)
+	@orders		{Array<Array>}	Optional. The same behavior as parameters (see the remarks)
   @return {any}		{ count: { count: # }, list: [{},{}...] }
 	
 	@remarks Optionally IManagedDataSupplier can be obtained from IManagedDataProvider. If so the provider [should] accept parameters and set them as initial parameters
@@ -11,5 +12,16 @@ IManagedDataSupplier.Interface("IManagedDataSupplier",IManagedInterface);
 			a few ticks and do not support changing the parameters over the supplier. In this case the supplier [must] throw an excpetion if parameters are passed to the call to GetRecords.
 			If IManagedDataProvider is implemented and used it [must] support parameters - it is not an optional feature. The provider can consume some of the parameters in order to determine 
 			what/which supplier to return.
+			Orders are given as an array containing arrays with 0-el the name of the field, 1-st el the direction (positive/negative number)
+			[[fn1,1],[fn2,-1]]
+			
 */
-IManagedDataSupplier.prototype.GetRecords = function(offset, limit, parameters) { throw "not impl";}; 
+IManagedDataSupplier.prototype.GetRecords = function(offset, limit, parameters, orders) { throw "not impl";}; 
+/**
+	@return {boolean}	True if parameters are supported
+*/
+IManagedDataSupplier.prototype.supportsParametrs = function() { throw "not impl"; }
+/**
+	@return {boolean}	True if orders are supported
+*/
+IManagedDataSupplier.prototype.supportsOrdering = function() { throw "not impl"; }
