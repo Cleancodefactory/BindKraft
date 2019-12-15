3b2d12e4de8943c8b104202323c97b56d077849c
Michael Palazov <michael@newobjects.com>
2019-12-15 23:23:06 +0200

    A few little improvements to ServiceFetcher and InitializeFetcher.

--- Scripts/core/initializers/InitializeFetcher.js
+++ Scripts/core/initializers/InitializeFetcher.js
@@ -1,12 +1,13 @@
 
 /*CLASS*/
-function InitializeFetcher(desc, iface, reason) {
+function InitializeFetcher(desc, iface, reason, silent) {
     Initialize.apply(this, arguments);
     this.type = "Fetcher";
 	this.active = true;
 	this.defaultValue = "ServiceFetcher";
     this.fetchInterface = iface;
 	this.fetchReason = reason;
+	this.fetchSilent = silent;
 };
 InitializeFetcher.Inherit(Initialize, "InitializeFetcher");
 InitializeFetcher.prototype.produceDefaultValue = function (obj) {
@@ -14,12 +15,12 @@ InitializeFetcher.prototype.produceDefaultValue = function (obj) {
 	var iface_name = Class.getTypeName(this.fetchInterface);
 	var reason = this.fetchReason;
 	if (iface_name == null) throw "InitializeFetcher requires interface argument e.g. new InitializeFetcher('description', ISomeInterface)"
-	return ServiceFetcher.createFetcher(obj, iface_name, reason);
+	return ServiceFetcher.createFetcher(obj, iface_name, reason, this.fetchSilent);
 };
 InitializeFetcher.prototype.defValueDescription = function () {
 	var iface_name = Class.getTypeName(this.fetchInterface) || "unknown";
     return {
         value: "",
-        type: "ServiceFetcher(" + iface_name + ")"
+        type: "ServiceFetcher(" + iface_name + ") " + (this.fetchSilent?"silent":"nonsilent")
     };
 };
