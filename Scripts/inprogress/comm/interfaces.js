98ff22de6da14dea4a4da4eaf2650bf1c22899e9
Robert Tanev <rtanev@cleancodefactory.de>
2019-12-19 21:29:58 +0200

    Starting prototyping

---
+++ Scripts/inprogress/comm/interfaces.js
@@ -0,0 +1,22 @@
+//Transport manager
+function ICommTransportManager(){}
+ICommTransportManager.Interface("ICommTransportManager");
+ICommTransportManager.prototype.requestSession = function(address){throw "not impl";} //:Operation<ITransportSession>
+
+//Transport session
+function ITransportSession(){}
+ITransportSession.Interface("ITransportSession");
+
+//
+
+
+this.requestSession().send();
+
+//time1
+this.container = this.requestBatchSession();
+this.container.add
+
+this.container.ajax("ip:verb");
+
+
+var operation = this.ajax("ip:verb", this.getdata());
