4e4e21132934827694b36e5da13825291c458a6c
Michael Palazov <michael@newobjects.com>
2020-09-13 16:09:03 +0300

    Add check for own property in objectyfyToAs

--- Scripts/core/interfaces/IObjectifiable.js
+++ Scripts/core/interfaces/IObjectifiable.js
@@ -51,7 +51,7 @@ IObjectifiable.objectifyToAs = function(o,inst,cls) {
 				o.$__className = inst.classType();
 			}
 			for (var i in x) {
-				if (i != "$__className") {
+				if (o.hasOwnProperty(i) && i != "$__className") {
 					o[i] = x[i];
 				}
 			}
