4a4bb7f6c3c2de30d6f6cf116968afbda14316c8
Michael Palazov <mpalazov@cleancodefactory.de>
2020-12-21 18:15:49 +0200

    version update

--- Scripts/core/configuration.js
+++ Scripts/core/configuration.js
@@ -9,7 +9,7 @@
 */
 
 var JBFrameworkVersion = { 
-	core: { major: 2, minor: 23, revision: 2 }
+	core: { major: 2, minor: 23, revision: 3 }
 };
 // Apply default settings, can be overriten (see in the header of the file how)
 (function() {
