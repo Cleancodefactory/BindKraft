d1b3482f58188605ede7481e0c74b3c24e0d1f45
Michael Palazov <mpalazov@cleancodefactory.de>
2020-12-29 18:11:05 +0200

    IsValueEqualToParameter

---
+++ Scripts/core/formatters/system/IsValueEqualToParameter.js
@@ -0,0 +1,32 @@
+(function() {
+
+    var SystemFormatterBase = Class("SystemFormatterBase");
+    var IArgumentListParserStdImpl = InterfaceImplementer("IArgumentListParserStdImpl");
+
+/**
+ * Converts empty values to null in the both direction
+ * By default treats the value as string or uses its toString form to determine if it is empty.
+ * Parameters number or bool can be passed to do this for numbers or booleans.
+ */
+function IsValueEqualToParameter() {
+	SystemFormatterBase.apply(this,arguments);
+}
+IsValueEqualToParameter.Inherit(SystemFormatterBase,"IsValueEqualToParameter");
+IsValueEqualToParameter.Implement(IArgumentListParserStdImpl,"trim");
+
+IsValueEqualToParameter.prototype.Read = function(val, bind, params) {
+    if (bind != null) {
+        return (val == bind.bindingParameter);
+    } else {
+        return false;
+    }
+}
+IsValueEqualToParameter.prototype.Write = function(val, bind, params) {
+    if (bind != null) {
+        return (val == bind.bindingParameter);
+    } else {
+        return false;
+    }
+}
+
+})();
