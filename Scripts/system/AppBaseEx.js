5a5c39ba32db4905d0beb453b0ab4f27142441de
Michael Palazov <mpalazov@cleancodefactory.de>
2021-01-26 16:58:45 +0200

    fixed AppBaseEx arguments transferred to initialize

--- Scripts/system/AppBaseEx.js
+++ Scripts/system/AppBaseEx.js
@@ -12,9 +12,9 @@ AppBaseEx.Inherit(AppBase,"AppBaseEx");
  * @param {*} args	The parameters passed to the application
  * @returns {Operation} to signal when the initialization is complete
  */
-AppBaseEx.prototype.initialize = function(/* *[] */ args) { throw "Not implemented";}
-AppBaseEx.prototype.appinitialize = function(callback, args) {
-	var op = this.initialize.apply(this, args);
+AppBaseEx.prototype.initialize = function(/* callset */ args) { throw "Not implemented";}
+AppBaseEx.prototype.appinitialize = function(callback, /* callset */ args) {
+	var op = this.initialize.apply(this, Array.createCopyOf(arguments, 1));
 	var me = this;
 	op.then(function(o) {
 		if (o.isOperationSuccessful()) {
@@ -27,7 +27,7 @@ AppBaseEx.prototype.appinitialize = function(callback, args) {
 /**
  *  Override only if needed.
  */
-AppBaseEx.prototype.run = function(/* [] */ args) { /* no mandatory content */ }
+AppBaseEx.prototype.run = function(/* callset */ args) { /* no mandatory content */ }
  
  /**
  * Override! 
