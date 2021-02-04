b753a146760b7ea1ad8c56ee7af059a0f56f8fc0
Michael Palazov <mpalazov@cleancodefactory.de>
2021-02-04 14:23:51 +0200

    ...

--- Scripts/system/shellcustomizers/MobileFirstCustomizer.js
+++ Scripts/system/shellcustomizers/MobileFirstCustomizer.js
@@ -1,5 +1,6 @@
 (function(){
-    var IShellCustomizerPlacer = Interface("IShellCustomizerPlacer");
+    var IShellCustomizerPlacer = Interface("IShellCustomizerPlacer"),
+        GRect = Class("GRect");
 
     function MobileFirstCustomizer() {
         BaseObject.apply(this, arguments);
@@ -9,14 +10,19 @@
         .ImplementProperty("shell")
         .ImplementProperty("workspacewindow");
         
-
+    MobileFirstCustomizer.prototype.$lastPos = 0;
     MobileFirstCustomizer.prototype.placeWindow = function(wnd, options) { 
-        if (options != null && options.role == "shell") {
-            if (typeof options.position == "string") {
-                this.$shell.workspaceWindow.addChild(w,options.position);
-            }
+        var ww = Shell.get_workspacewindow();
+        var slot = ww.get_clientcontainer("right");
+        var rect = GRect.fromDOMElementClient(slot);
+        rect.y += this.$lastPos;
+        this.$lastPos += 30;
+        rect = new Rect(rect.l, rect.y,rect.w,rect.h);
+        if (options != null) {
+            this.$shell.workspaceWindow.addChild(wnd);
         } else {
-            this.$shell.workspaceWindow.addChild(w);
+            this.$shell.workspaceWindow.addChild(wnd, "right");
+            wnd.set_windowrect(rect);
         }
     }
     MobileFirstCustomizer.prototype.displaceWindow = function(wnd) { 
