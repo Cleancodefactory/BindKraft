02aee6fc9a9c0cf3c62fb13936dd5447752b705e
Michael Palazov <mpalazov@cleancodefactory.de>
2021-02-09 19:44:49 +0200

    ...

--- Scripts/windowing/workspace/WorkspaceWindowBase.js
+++ Scripts/windowing/workspace/WorkspaceWindowBase.js
@@ -7,4 +7,17 @@
 
     WorkspaceWindowBase.prototype.$defaultWindowStyles = WindowStyleFlags.fillparent | WindowStyleFlags.visible | WindowStyleFlags.adjustclient;
 
+    WorkspaceWindowBase.prototype.$activateWindowFromDOMEvent = function (e) {
+        //e.stopPropagation();
+    };
+    WorkspaceWindowBase.prototype.on_ActivateWindow = function (msg) {
+        //e.stopPropagation();
+        this.updateTargets();
+        msg.handled = true;
+    };
+    WorkspaceWindow.prototype.on_Create = function () {
+        this.springTrigger = new SpringTrigger(new Delegate(this, this.onBrowserResize), 100);
+        $(window).resize(Delegate.createWrapper(this, this.windUp));
+    
+    };
 })();
