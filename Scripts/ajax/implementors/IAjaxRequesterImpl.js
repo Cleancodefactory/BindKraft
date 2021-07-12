fab27b81f10521f877d902db23a46a4ecf8a0519
Michael Palazov <mpalazov@cleancodefactory.de>
2021-07-12 20:15:45 +0300

    ...

--- Scripts/ajax/implementors/IAjaxRequesterImpl.js
+++ Scripts/ajax/implementors/IAjaxRequesterImpl.js
@@ -2,12 +2,25 @@
     function IAjaxRequesterImpl() {}
     IAjaxRequesterImpl.InterfaceImpl(IAjaxRequester);
 
-    IAjaxRequesterImpl.classInitialize = function(cls) {
+    IAjaxRequesterImpl.classInitialize = function(cls, _defaultPriority) {
+        var defaultPriority = _defaultPriority || 0;
+        function _enqueueRequest(req) {
+            AjaxSendQueue.Default().enqueueRequest(req, defaultPriority);
+        }
+
+
         cls.prototype.ajaxSendRequest = function(url_or_req, data_or_reqdata_or_callback, callback, cache) {
+
             if (BaseObject.is(url_or_req, "IAjaxRequest")) {
                 var request = url_or_req;
                 if (BaseObject.isCallback(data_or_reqdata_or_callback)) {
-
+                    if (typeof callback == "boolean") {
+                        request.set_cache(callback);
+                    }
+                    request.completeRequest = function(response) {
+                        BaseObject.callCallback(data_or_reqdata_or_callback, response);
+                    }
+                    _enqueueRequest(request);
                 } else {
                     if (typeof data_or_reqdata_or_callback == "boolean") {
                         request.set_cache(data_or_reqdata_or_callback);
@@ -15,13 +28,24 @@
                     var op = new Operation(); // TODO: Needs some safe timeouts etc.
                     request.completeRequest = function(response) {
                         if (BaseObject.is(response, "IAjaxResponse")) {
-                            ///////////
+                            if (response.get_success()) {
+                                op.CompleteOperation(true, response);
+                            } else {
+                                op.CompleteOperation(false, response.get_message);
+                            }
                         } else {
-                            ////////
+                            op.CompleteOperation(false, "" + response);
                         }
                     }
+                    _enqueueRequest(request);
+                    return op;
                 }
+                
+            } else {
+                // Build a request ourselves
+                
             }
         }
+
     }
 })();
