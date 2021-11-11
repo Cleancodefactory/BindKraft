2e33558a4de28c656ff5c88c71655a40c04cf4f7
Michael Palazov <mpalazov@cleancodefactory.de>
2021-11-11 21:55:40 +0200

    Added support for multiple in Async And Delegate validators

--- Scripts/view/validatorrules/AsyncValidatorRule.js
+++ Scripts/view/validatorrules/AsyncValidatorRule.js
@@ -5,10 +5,11 @@ function AsyncValidatorRule(v) {
 	this.$order = 10000;
 };
 
-AsyncValidatorRule.Inherit(ValidateValue, "AsyncValidatorRule");
+AsyncValidatorRule.Inherit(ValidateValues, "AsyncValidatorRule");
 AsyncValidatorRule.registerValidator("async");
 AsyncValidatorRule.prototype.$validator = null;
 AsyncValidatorRule.prototype.get_delegate = function () { return null; };
+AsyncValidatorRule.prototype.get_simpledelegate = function () { return null; };
 AsyncValidatorRule.prototype.get_param1 = function () { return null; };
 AsyncValidatorRule.prototype.get_param2 = function () { return null; };
 AsyncValidatorRule.prototype.get_param3 = function () { return null; };
@@ -18,11 +19,22 @@ AsyncValidatorRule.prototype.get_message = function (lastValue) {
 	if (msg != null && msg.length > 0) msg = msg.sprintf(lastValue);
 	return msg;
 };
-AsyncValidatorRule.prototype.validateValue = function (validator, value, binding) {
+AsyncValidatorRule.prototype.validateValue = function (validator, values, bindings) {
 	var result = ValidationResultEnum.correct;
+	if (!Array.isArray(binings)) return this.validationResult(result);
+	bindings = Array.createCopyOf(bindings);
+	if (Array.isArray(values)) values = Array.createCopyOf(values);
+
 	var d = this.get_delegate();
-	if (this.get_nocheck() || (d != null && !IsNull(value))) {
-		var r = d.invoke({ rule: this, validator: validator, value: value, binding: binding });
+	var sd = this.get_simpledelegate();
+
+	if (d != null || sd != null) {
+		var r;
+		if (d != null) {
+			r = d.invoke({ rule: this, validator: validator, values: values, bindings: bindings });
+		} else {
+			r = sd.invoke(this, validator, values, bindings );
+		}
 		if (r > result) result = r;
 		if (r == ValidationResultEnum.pending) return r;
 	}
--- Scripts/view/validatorrules/DelegateValidatorRule.js
+++ Scripts/view/validatorrules/DelegateValidatorRule.js
@@ -5,10 +5,11 @@ function DelegateValidatorRule(v) {
 	this.$order = 10000;
 };
 
-DelegateValidatorRule.Inherit(ValidateValue, "DelegateValidatorRule");
+DelegateValidatorRule.Inherit(ValidateValues, "DelegateValidatorRule");
 DelegateValidatorRule.registerValidator("sync");
 DelegateValidatorRule.prototype.$validator = null;
 DelegateValidatorRule.prototype.get_delegate = function () { return null; };
+DelegateValidatorRule.prototype.get_simpledelegate = function () { return null; };
 DelegateValidatorRule.prototype.get_param1 = function () { return null; };
 DelegateValidatorRule.prototype.get_param2 = function () { return null; };
 DelegateValidatorRule.prototype.get_param3 = function () { return null; };
@@ -20,9 +21,32 @@ DelegateValidatorRule.prototype.get_message = function (lastValue) {
 
 DelegateValidatorRule.prototype.validateValue = function (validator, value, binding) {
 	var result = ValidationResultEnum.correct;
+	
 	var d = this.get_delegate();
-	if (d != null && !IsNull(value)) {
-		return d.invoke({ rule: this, validator: validator, value: value, binding: binding });
+	var sd = this.get_simpledelegate();
+	if ((sd != null || d != null) && !this.isValueEmpty(value)) {
+		if (d != null) {
+			result = d.invoke({ rule: this, validator: validator, value: value, binding: binding });
+		} else if (sd != null) {
+			result = sd.invoke(this, validator, value, binding);
+		}
+		
+	}
+	return this.validationResult(result);
+};
+DelegateValidatorRule.prototype.validateValues = function (validator, values, bindings) {
+	var result = ValidationResultEnum.correct;
+	if (!Array.isArray(binings)) return this.validationResult(result);
+	bindings = Array.createCopyOf(bindings);
+	if (Array.isArray(values)) values = Array.createCopyOf(values);
+	var d = this.get_delegate();
+	var sd = this.get_simpledelegate();
+	if (d != null || sd != null) {
+		if (d != null) {
+			result = d.invoke({ rule: this, validator: validator, values: values, bindings: bindings });
+		} else {
+			result = sd.invoke(this, validator, values, bindings );
+		}
 	}
 	return this.validationResult(result);
 };
