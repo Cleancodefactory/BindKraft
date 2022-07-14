64c4e0f44dad7ef80c36acc4e637f9136482ecb7
Michael Palazov <mpalazov@cleancodefactory.de>
2022-07-14 20:30:06 +0300

    ...

--- Scripts/view/validatorrules/RequiredFieldValidatorRule.js
+++ Scripts/view/validatorrules/RequiredFieldValidatorRule.js
@@ -14,10 +14,23 @@ RequiredFieldValidatorRule.prototype.get_message = function (lastValue) {
 	}
 	return msg;
 };
+RequiredFieldValidatorRule.ImplementProperty("hasitems", new InitializeBooleanParameter("If the value is array or object - it must NOT be empty."))
 RequiredFieldValidatorRule.prototype.validateValue = function (validator, value, binding) {
 	var result = ValidationResultEnum.correct;
 	if (IsNull(value) || value.toString().trim().length == 0) {
 		result = ValidationResultEnum.incorrect;
+	} else if (this.get_hasitems()) {
+		if (Array.isArray(value)) {
+			if (value.length == 0) result = ValidationResultEnum.incorrect;
+		} else if (typeof value == "object") {
+			result = ValidationResultEnum.incorrect;
+			for (var key in value) {
+				if (value.hasOwnProperty(key)) {
+					result = ValidationResultEnum.correct;
+					break;
+				}
+			}
+		}
 	}
 	return this.validationResult(result);
 };
