9d0d05fbe421358bf56bc7965119d630208aa4e7
Michael Palazov <mpalazov@cleancodefactory.de>
2022-07-18 18:04:02 +0300

    utf8

--- Scripts/core/misc/utf8.js
+++ Scripts/core/misc/utf8.js
@@ -1,61 +1,75 @@
-var Utf8 = {
- 
-	// public method for url encoding
-	encode : function (string) {
-		string = string.replace(/\r\n/g,"\n");
-		var utftext = "";
- 
-		for (var n = 0; n < string.length; n++) {
- 
-			var c = string.charCodeAt(n);
- 
-			if (c < 128) {
-				utftext += String.fromCharCode(c);
-			}
-			else if((c > 127) && (c < 2048)) {
-				utftext += String.fromCharCode((c >> 6) | 192);
-				utftext += String.fromCharCode((c & 63) | 128);
-			}
-			else {
-				utftext += String.fromCharCode((c >> 12) | 224);
-				utftext += String.fromCharCode(((c >> 6) & 63) | 128);
-				utftext += String.fromCharCode((c & 63) | 128);
-			}
- 
-		}
- 
-		return utftext;
-	},
- 
-	// public method for url decoding
-	decode : function (utftext) {
-		var string = "";
-		var i = 0;
-		var c = c1 = c2 = 0;
- 
-		while ( i < utftext.length ) {
- 
-			c = utftext.charCodeAt(i);
- 
-			if (c < 128) {
-				string += String.fromCharCode(c);
-				i++;
-			}
-			else if((c > 191) && (c < 224)) {
-				c2 = utftext.charCodeAt(i+1);
-				string += String.fromCharCode(((c & 31) << 6) | (c2 & 63));
-				i += 2;
-			}
-			else {
-				c2 = utftext.charCodeAt(i+1);
-				c3 = utftext.charCodeAt(i+2);
-				string += String.fromCharCode(((c & 15) << 12) | ((c2 & 63) << 6) | (c3 & 63));
-				i += 3;
-			}
- 
-		}
- 
-		return string;
+(function(){
+	function UTF8Encoding() {
+		BaseObject.apply(this,arguments);
 	}
- 
-}
+	UTF8Encoding.Inherit(BaseObject, "UTF8Encoding");
+
+	//#region Static methods
+	UTF8Encoding.encodeToBinaryString = function(input) { 
+		var c;
+		input = input + "";
+		if (typeof input == "string") {
+			var bstr = "";
+ 			for (var i = 0; i < input.length; i++) {
+				c = input.charCodeAt(i);
+				if (c < 0x80) { // ASCII - single byte
+					bstr += String.fromCharCode(c);
+				} else if((c >= 0x80) && (c < 0x800)) { // 2 bytes
+					bstr += String.fromCharCode((c >> 6) | 0xC0); // hi 5 bits
+					bstr += String.fromCharCode((c & 0x3F) | 0x80); // low 6 bits
+				} else if (c >= 0x800 && c < 0x10000) { // 3 bytes
+					bstr += String.fromCharCode((c >> 0xC) | 0xE0); // hi 4 bits
+					bstr += String.fromCharCode(((c >> 6) & 0x3F) | 0x80); // middle 6 bits
+					bstr += String.fromCharCode((c & 0x3F) | 0x80); // low 6 bits
+				} else if (c >= 0x10000 && c <= 0x10FFFF) {
+					bstr += String.fromCharCode((c >> 0x12) | 0xF0); // hi 3 bits
+					bstr += String.fromCharCode(((c >> 12) & 0x3F) | 0x80); // hi low 6 bits
+					bstr += String.fromCharCode(((c >> 6) & 0x3F) | 0x80); // low hi 6 bits
+					bstr += String.fromCharCode((c & 0x3F) | 0x80); // low 6 bits
+				} else {
+					BaseObject.LASTERROR("Invalid code point");
+					return null;
+				}
+			}
+			return bstr;
+		}
+		BaseObject.LASTERROR("Invalid input");
+		return null;
+	}
+	UTF8Encoding.decodeFromBinaryString = function(binput) {
+		if (typeof binput == "string") {
+			var s = ""; // output
+			var cp,c,i = 0;
+			while (i < binput.length) {
+				c = binput.charCodeAt(i++);
+				if (c < 0x80) {
+					s += String.fromCharCode(c);
+				} else if (c >= 0x80) { // Multibyte character
+					if (c >= 0xF0) { // 4 bytes
+						cp = (c & 0x07) << 18;
+						cp = cp | ((binput.charCodeAt(i++) & 0x3F) << 12);
+						cp = cp | ((binput.charCodeAt(i++) & 0x3F) << 6);
+						cp = cp | (binput.charCodeAt(i++) & 0x3F);
+					} else if (c >= 0xE0 && c < 0xF0) {// 3 bytes
+						cp = (c & 0x0F) << 12;
+						cp = cp | ((binput.charCodeAt(i++) & 0x3F) << 6);
+						cp = cp | (binput.charCodeAt(i++) & 0x3F);
+					} else if (c >= 0xC0 && c < 0xE0) { //2 bytes
+						cp = (c & 0x1F) << 6;
+						cp = cp | (binput.charCodeAt(i++) & 0x3F);
+					} else {
+						BaseObject.LASTERROR("Unrecognized UTF 8 character - " + c);
+						cp = 0;
+					}
+					if (cp > 0) {
+						s += String.fromCharCode(cp);
+					}
+				}
+			}
+			return s;
+		}
+		return null;
+	}
+
+})();
+
