5d978e07d5c4dcfe73f7a977c41b48cc63ffc28d
Michael Palazov <mpalazov@cleancodefactory.de>
2022-08-02 18:58:20 +0300

    Added system formatter TranslateThrough

---
+++ Scripts/core/formatters/system/TranslateThrough.js
@@ -0,0 +1,31 @@
+(function() {
+
+    var SystemFormatterBase = Class("SystemFormatterBase");
+    var IArgumentListParserStdImpl = InterfaceImplementer("IArgumentListParserStdImpl"),
+        Localization = Class("Localization");
+
+    /**
+     * Converts empty values to null in the both direction
+     * By default treats the value as string or uses its toString form to determine if it is empty.
+     * Parameters number or bool can be passed to do this for numbers or booleans.
+     */
+    function TranslateThrough() {
+        SystemFormatterBase.apply(this,arguments);
+    }
+    TranslateThrough.Inherit(SystemFormatterBase,"TranslateThrough");
+    TranslateThrough.Implement(IArgumentListParserStdImpl,"trim");
+
+    TranslateThrough.prototype.Read = function(val, bind, params) {
+        var translations = Localization.get_translation(params);
+        if (translations != null) {
+            return BaseObject.GetProperty(val, null);
+        } else {
+            this.LASTERROR("Translations for the class " + params + " was not found. Check if it is loaded.");
+            return null;
+        }
+    }
+    TranslateThrough.prototype.Write = function(val, bind, params) {
+        throw "TranslateThrough formatter works only in read direction (ToTarget)";
+    }
+
+})();
