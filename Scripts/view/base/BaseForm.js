bfe890e06308e5e0237dfc529d6e6a2167339a33
Michael Palazov <mpalazov@cleancodefactory.de>
2023-03-07 21:44:23 +0200

    ...

--- Scripts/view/base/BaseForm.js
+++ Scripts/view/base/BaseForm.js
@@ -1,6 +1,6 @@
 (function() {
     /**
-     * Use instead of Base to create part validated separately
+     * Use instead of Base to create part validated separately or use as a base class of controls (only very specific ones - usually you will start from Control or even Base)
      */
     function BaseForm() {
         Base.apply(this, arguments);
