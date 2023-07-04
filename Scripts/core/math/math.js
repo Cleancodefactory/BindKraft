fdd7a9ae41a7576cf0d44f3c3ae1d73b9eeb4ac8
Michael Palazov <michael@newobjects.com>
2023-07-04 21:52:12 +0300

    ...

--- Scripts/core/math/math.js
+++ Scripts/core/math/math.js
@@ -6,16 +6,39 @@
 */
 
 //// MATH HELPERS /////////////////////	
-	
+/**
+ * 
+ * @param {int} v - value to manipulate as bit flags
+ * @param {*} flags to set
+ * @returns the value with the specified flags set
+ */
 Math.bitsOn = function (v, flags) {
     return (v | flags);
 };
+/**
+ * 
+ * @param {int} v Value to manipulate as flags
+ * @param {int} flags to clear 
+ * @returns returns thevalue 'v' wit the 'flags' cleared
+ */
 Math.bitsOff = function (v, flags) {
     return (v & (~flags));
 };
+/**
+ * 
+ * @param {int} v to manipulate as flags 
+ * @param {int} flags to test for
+ * @returns {boolean} true if the specified 'flags' are set, false if not all of them are set
+ */
 Math.bitsTest = function (v, flags) {
     return ((v & flags) == flags);
 };
+/**
+ * 
+ * @param {int} v value to manipulate as flags 
+ * @param {int} mask to apply 
+ * @returns {int} the value masked to the specified mask
+ */
 Math.bitsMasked = function(v, mask) {
 	return (v & mask);
 }
