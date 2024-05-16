40c91dbf5a8d65df442332696c9ff1d57b733d23
Michael Palazov <mpalazov@cleancodefactory.de>
2024-05-16 16:14:30 +0300

    ...

--- Scripts/view/controls/LookupTest.js
+++ Scripts/view/controls/LookupTest.js
@@ -14,14 +14,15 @@
     }
     LookupTest.ImplementInterfaceBubble("lookupcallback", ILookupInputCallback,{
         getChoices: function(flt) {
-            return [
-                { val:"AAA",desc: "Item A"},
-                { val:"BB",desc: "Item B"},
-                { val:"CCC",desc: "Item C"}
-            ].filter(o => flt == null || flt == "" || o.val.indexOf(flt) >= 0);
+            return ["00","15","30","45"].filter(i => flt == null || flt.length == 0 || i.indexOf(flt) >= 0);
+            // return [
+            //     { val:"AAA",desc: "Item A"},
+            //     { val:"BB",desc: "Item B"},
+            //     { val:"CCC",desc: "Item C"}
+            // ].filter(o => flt == null || flt == "" || o.val.indexOf(flt) >= 0);
         },
         makeSelection: function(choice) {
-            return choice.val;
+            return choice;
         }
     })
 
